d0cb6d7971b444fe7c729edd09c0e7f707965b4e
AgenciaSeduza <agenciaseduza@gmail.com>
2025-12-03 20:31:53 -0300

    Update tiktok-event.js

--- netlify/functions/tiktok-event.js
+++ netlify/functions/tiktok-event.js
@@ -1,6 +1,20 @@
 import fetch from "node-fetch";
 
+export const handler = async (event) => {
+
   // ✅ Só aceita POST
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: "OK"
+    };
+  }
+
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
@@ -22,6 +36,7 @@ import fetch from "node-fetch";
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        // ⚠️ TROQUE POR UM NOVO TOKEN
         "Access-Token": "0322d0268e31b9d59d2690d697389a2aa704d681"
       },
       body: JSON.stringify(data)
@@ -37,6 +52,11 @@ import fetch from "node-fetch";
   } catch (error) {
     return {
       statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
+
       body: JSON.stringify({ error: error.message })
     };
   }
